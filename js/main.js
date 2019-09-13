f287c4b719a56f204091a045d1659d378c9ae243
vumg <ab17100222@gmail.com>
2019-09-13 17:52:08 -0500

    final layout

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ let proyectos = {
 
   getProyectos: function() {
     let proyectos = document.querySelectorAll(".proyecto");
-
+    
     proyectos.forEach(element => {
       this.proyectos.push(element);
     });
@@ -28,18 +28,29 @@ let proyectos = {
 
   clickEvent: function(proyecto, num) {
     proyecto.addEventListener("click", () => {
-      atras.mostrar();
-
       switch (num) {
         case 0:
           this.desaparecerProyectos(0);
-          screenshots.mostrar(0);  
+          screenshots.mostrar(0);
+          tecnologias.mostrar(0);
+          atras.mostrar(0);
+          botones.mostrar(0);
           break;
         case 1:
           this.desaparecerProyectos(1);
+          screenshots.mostrar(1);
+          tecnologias.mostrar(1);
+          atras.mostrar(1);
+          this.animation2(this.proyectos[num], 0, 0, 1, 1,"40%","13%")
+          botones.mostrar(1);
           break;
         case 2:
           this.desaparecerProyectos(2);
+          atras.mostrar(2);
+          screenshots.mostrar(2);
+          tecnologias.mostrar(2);
+          this.animation2(this.proyectos[num], 0, 0, 1, 1,"65%","13%")
+          botones.mostrar(2);
           break;
       }
     });
@@ -48,26 +59,30 @@ let proyectos = {
   desaparecerProyectos: function(proyecto) {
     this.proyectos.forEach((element, num) => {
       if (num != proyecto) {
-        this.animation2(element);
+        element.classList.remove("show");
       }
     });
   },
 
-  animation2: function(proyecto) {
+  animation2: function(proyecto, x1, x2, opacity1, opacity2, y1,y2) {
     anime({
       targets: proyecto,
-      translateX: [0, -500],
-      opacity: [1, 0],
-      duration: 3000
+      translateX: [x1, x2],
+      opacity: [opacity1, opacity2],
+      duration: 300,
+      easing: "linear",
+      top: [y1,y2]
     });
   }
 };
 
 let atras = {
-  mostrar: function() {
+  mostrar: function(proyecto) {
     let atras = document.querySelector(".atras");
     atras.classList.add("show");
+
     this.animation(atras);
+  
   },
   animation: function(atras) {
     anime({
@@ -78,7 +93,8 @@ let atras = {
       delay: 200,
       easing: "linear"
     });
-  }
+  },
+
 };
 
 let screenshots = {
@@ -92,7 +108,7 @@ let screenshots = {
     });
 
     this.ss[num].classList.add("show");
-    this.animation(this.ss[num]); 
+    this.animation(this.ss[num]);
   },
 
   animation: function(ss) {
@@ -104,9 +120,49 @@ let screenshots = {
       delay: 200,
       easing: "linear"
     });
+  },
+  
+};
+
+let tecnologias = {
+
+  tecnologias: [],
+
+  mostrar: function(num) {
+    let tecnologias = document.querySelectorAll(".tecnologia");
+    
+    tecnologias.forEach((element) => {
+      this.tecnologias.push(element);
+    })
+
+    this.tecnologias[num].classList.add("show");
+    this.animation(tecnologias[num], 0, 0 , 0, 1)
+  },
+
+  animation: function(proyecto, x1, x2, opacity1, opacity2) {
+    anime({
+      targets: proyecto,
+      translateX: [x1, x2],
+      opacity: [opacity1, opacity2],
+      duration: 300,
+      easing: "linear"
+    });
   }
 };
 
+let botones = {
+  botones: [],
+
+  mostrar: function(num){
+    let botones = document.querySelectorAll(".learn-more");
+
+    botones.forEach((element) => {
+      this.botones.push(element);
+    })
+
+    this.botones[num].classList.add("show");
+  }
+}
 proyectos.getProyectos();
 
 setTimeout(() => {
